bdffd5b1502e34474fcc620cfedf4745f2250fba
Karan Singh <kss4382@gmail.com>
2020-12-07 13:04:57 -0500

    ready to deploy

--- src/app/mobile-nav/mobile-nav.component.ts
+++ src/app/mobile-nav/mobile-nav.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
-import { state, style } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+
+import { NavigationStart, Router, RouterEvent } from '@angular/router';
+
+import {  } from 'rxjs/operators';
 
 @Component({
   selector: 'app-mobile-nav',
   templateUrl: './mobile-nav.component.html',
   styleUrls: ['./mobile-nav.component.scss'],
 })
-export class MobileNavComponent {
-  status:boolean = false;
-  constructor() { }
+export class MobileNavComponent implements OnInit {
+  constructor(private router: Router) { 
+   }
+   // status starts off as true because onInit the router subscription below will change it to false
+  status: boolean = true;
+
+   ngOnInit() {
+    // to close the sidebar everytime the user navigates away from page
+    this.router.events.subscribe((event: RouterEvent) => {
+      if(event instanceof NavigationStart) {
+        this.status = !this.status;
+        console.log(this.status)
+      }
+    })
+   }
 
   onClick(data) {
-    //testing
-    console.log(data);
     this.status = !this.status;
   }
 }
